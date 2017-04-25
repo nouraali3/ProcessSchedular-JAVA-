f6eca1155cb4939b7f1958705f98c7f7eb4c1597
nouraali3 <noura.ali9@yahoo.com>
2017-04-25 13:45:58 +0200

    without gantt chart in GUI

---
+++ guitrial/src/guitrial/Guitrial.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guitrial;
+
+/**
+ *
+ * @author user
+ */
+public class Guitrial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        z1 z=new z1();
+       z.setVisible(true);
+    }
+    
+}
